761a6e07bd8adf9b49ba1bb722a2c4c81780f5e3
Martin Bednář <31645517+m-bednar@users.noreply.github.com>
2020-10-18 21:04:32 +0200

    Update error.h

--- src/error.h
+++ src/error.h
@@ -7,8 +7,8 @@
 #pragma once
 
 #include <stdio.h>
-#include "errcodes.h"
 
+// Error codes
 #define ERRCODE_INTERNAL_ERROR 99
 
 #define error(msg) fprintf(stderr, "Error at %s:%d: %s\n", __FILE__, __LINE__, msg); exit(ERRCODE_INTERNAL_ERROR)
