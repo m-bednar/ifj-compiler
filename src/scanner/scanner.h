996e60bcfc08305dbbb8533acb44ba40b546d60f
ItsFreSsh <lukes.fr@seznam.cz>
2020-10-18 20:22:37 +0200

    Added scanner interface

--- src/scanner/scanner.h
+++ src/scanner/scanner.h
@@ -5,3 +5,17 @@
  */
 
 #pragma once
+
+typedef enum tokenid {
+   TOKENID_SPACE,
+   TOKENID_NEWLINE,
+   TOKENID_INDENT,
+   TOKENID_IDENTIFIER,
+   TOKENID_KEYWORD
+} tokenid;
+
+typedef struct token_t {
+   tokenid id;
+} token_t;
+
+token_t* get_next_token();
