ec74b7224656efe10ab4342bbb79cff27c1676e9
Martin Bednář <31645517+m-bednar@users.noreply.github.com>
2020-10-18 21:10:11 +0200

    Code removed from main.c

--- src/main.c
+++ src/main.c
@@ -4,13 +4,6 @@
  * @authors Martin Bednář (xbedna77)
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "symtable/symtable.h"
-
 int main() {
-   bintree_t* tree = bintree_ctor();
-
-   bintree_dtor(tree);
    return EXIT_SUCCESS;
 }
