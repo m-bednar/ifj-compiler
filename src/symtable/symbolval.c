d6c4eb15ea56f5b351c2f443b18f5f59e1fee242
mar-bednar <mbedna8@seznam.cz>
2020-10-21 14:50:56 +0200

    Symbolval functions prototypes added

--- src/symtable/symbolval.c
+++ src/symtable/symbolval.c
@@ -4,4 +4,22 @@
  * @authors Martin Bednář (xbedna77)
  */
 
+#include <stdlib.h>
 #include "symbolval.h"
+
+
+symbolvalfn_t* symbolvalfn_ctor(int arg_count, vartype* arg_types, vartype return_type) {
+   return NULL;
+}
+
+symbolvalvar_t* symbolvalvar_ctor(vartype type) {
+   return NULL;
+}
+
+void symbolvalfn_dtor(symbolvalfn_t* symbolval) {
+
+}
+
+void symbolvalvar_dtor(symbolvalvar_t* symbolval) {
+
+}
--- src/symtable/symbolval.h
+++ src/symtable/symbolval.h
@@ -25,6 +25,27 @@ typedef struct symbolvalvar_t {
 } symbolvalvar_t;
 
 typedef union symbolval_u {
-   symbolvalfn_t fn;
-   symbolvalvar_t var;
+   symbolvalfn_t* fn;
+   symbolvalvar_t* var;
 } symbolval_u;
+
+
+/**
+ * Allocates and creates new symbol value for function.
+ */
+symbolvalfn_t* symbolvalfn_ctor(int arg_count, vartype* arg_types, vartype return_type);
+
+/**
+ * Allocates and creates new symbol value for variable.
+ */
+symbolvalvar_t* symbolvalvar_ctor(vartype type);
+
+/**
+ * Deallocates symbol function value.
+ */
+void symbolvalfn_dtor(symbolvalfn_t* symbolval);
+
+/**
+ * Deallocates symbol variable value.
+ */
+void symbolvalvar_dtor(symbolvalvar_t* symbolval);
