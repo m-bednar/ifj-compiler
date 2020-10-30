ba2580a7cfa06b869e78e214ca0b8570b2655f09
ItsFreSsh <lukes.fr@seznam.cz>
2020-10-30 15:31:25 +0100

    Changed datatypes, added token ids.

--- src/scanner/scanner.h
+++ src/scanner/scanner.h
@@ -4,37 +4,48 @@
  * @authors František Lukeš (xlukes07)
  */
 
+#include <stdint.h>
 #pragma once
 
 typedef enum tokenid {
-   TOKENID_SPACE,
-   TOKENID_NEWLINE,              // \n
-   TOKENID_INDENT,               // \t
+   //TOKENID_SPACE,
+   //TOKENID_INDENT,               // \t
    TOKENID_IDENTIFIER,
-   TOKENID_CONTROL_KEYWORD,      // if, for, else, return
-   TOKENID_DECLARATION_KEYWORD,  // package, func
-   TOKENID_DATATYPE,             // float64, int, string, bool
+   TOKENID_CONTROL_KEYWORD,         // if, for, else, return
+   TOKENID_DECLARATION_KEYWORD,     // package, func
+   TOKENID_DATATYPE,                // float64, int, string, bool
    TOKENID_NUM,
-   TOKENID_OPERATOR,             // +, -, *, /, <, <=, >, >=
-   TOKENID_DECLARATION_OPERATOR, // :=
-   TOKENID_ASSIGN_OPERATOR,      // =
-   TOKENID_LEFT_PARENTHESES,     // (
-   TOKENID_RIGHT_PARENTHESES,    // )
-   TOKENID_LEFT_BRACKET,         // {
-   ToKENID_RIGHT_BRACKET,        // }
-   TOKENID_END_OF_FILE           // EOF
+   TOKENID_NUM_DECIMAL,
+   TOKENID_OPERATOR_PLUS,           // +
+   TOKENID_OPERATOR_MINUS,          // -
+   TOKENID_OPERATOR_MULT,           // *
+   TOKENID_OPERATOR_DIV,            // /
+   TOKENID_OPERATOR_LESS,           // <
+   TOKENID_OPERATOR_LESS_OR_EQUAL,  // <=
+   TOKENID_OPERATOR_MORE,           // >
+   TOKENID_OPERATOR_MORE_OR_EQUAL,  // >=
+   TOKENID_DECLARATION_OPERATOR,    // :=
+   TOKENID_ASSIGN_OPERATOR,         // =
+   TOKENID_LEFT_PARENTHESES,        // (
+   TOKENID_RIGHT_PARENTHESES,       // )
+   TOKENID_LEFT_BRACKET,            // {
+   TOKENID_RIGHT_BRACKET,           // }
+   TOKENID_SEMICOLON,               // ;
+   TOKENID_COMMA,                   // ,
+   TOKENID_NEWLINE,                 // \n
+   TOKENID_END_OF_FILE              // EOF
 } tokenid;
 
-typedef union value_t {
-   char string_value[256];
-   int int_value;
-   float float_value;
-} value_t;
+typedef union value_u {
+   char* string_value;
+   int64_t int_value;
+   double float_value;
+} value_u;
 
 
 typedef struct token_t {
    tokenid id;
-   value_t value;
+   value_u value;
 } token_t;
 
 /**
