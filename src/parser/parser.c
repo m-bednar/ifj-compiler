dd056fb6e697c0363dd3483df49bd2f1399d9696
jackjack90s <xhavra18@stud.fit.vutbr.cz>
2020-10-30 13:39:32 +0100

    Parser files created

---
+++ src/parser/parser.c
@@ -0,0 +1,7 @@
+/**
+ * IFJ20 Compiler
+ * @file parser.c
+ * @authors Daniel Havranek (xhavra18)
+ */
+
+#include "parser.h"
---
+++ src/parser/parser.h
@@ -0,0 +1,5 @@
+/**
+ * IFJ20 Compiler
+ * @file parser.h
+ * @authors Daniel Havranek (xhavra18)
+ */
