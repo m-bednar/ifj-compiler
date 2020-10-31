b2d320fcfaf6d5ca568cdb909c6c3c2c659a5528
jackjack90s <xhavra18@stud.fit.vutbr.cz>
2020-10-31 15:37:03 +0100

    Added functions definition to ntsymbol.c

--- src/parser/ntsymbol.c
+++ src/parser/ntsymbol.c
@@ -5,3 +5,17 @@
  */
 
 #include "ntsymbol.h"
+
+ntsymbol_t* ntsymbol_ctor(ntsymid_t id) {
+    ntsymbol_t* ntsymbol = safe_alloc(sizeof(ntsymbol_t));
+
+    ntsymbol->id = id;
+    return ntsymbol;
+}
+
+void ntsymbol_dtor(ntsymbol_t* ntsymbol) {
+    guard(ntsymbol != NULL);
+
+    free(ntsymbol);
+    return;
+}
