8e334b2f1bbffeb6f8d128e32989501a93960642
jackjack90s <xhavra18@stud.fit.vutbr.cz>
2020-10-31 14:27:25 +0100

    Created parser stack element files

---
+++ src/parser/ntsymbol.c
@@ -0,0 +1,5 @@
+/**
+ * IFJ20 Compiler
+ * @file ntsymbol.c
+ * @authors Daniel Havranek (xhavra18)
+ */
---
+++ src/parser/ntsymbol.h
@@ -0,0 +1,7 @@
+/**
+ * IFJ20 Compiler
+ * @file ntsymbol.h
+ * @authors Daniel Havranek (xhavra18)
+ */
+
+#pragma once
