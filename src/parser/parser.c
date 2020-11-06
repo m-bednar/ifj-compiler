c2e4ee0976eb6ce0b218c2caebe547c035005234
jackjack90s <xhavra18@stud.fit.vutbr.cz>
2020-11-06 15:29:43 +0100

    Added first part of parsing algorithm

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -7,6 +7,18 @@
 #include "parser.h"
 
 void parse() {
-    
+    ntsymstack_t *stack = ntsymstack_ctor();
+
+    ntsymbol_t *end_of_file = ntsymbol_ctor(TOKENID_END_OF_FILE, true);
+    ntsymbol_t *program = ntsymbol_ctor(NONTERM_PROGRAM, false);
+
+    ntsymstack_push(stack, end_of_file);
+    ntsymstack_push(stack, program);
+
+    // parsing cycle
+
+    ntsymbol_dtor(program);
+    ntsymbol_dtor(end_of_file);
+   
     return;
 }
