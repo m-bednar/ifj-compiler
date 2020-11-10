efb07b5064a7e624a91ffaf3ded817530e88dce5
ItsFreSsh <lukes.fr@seznam.cz>
2020-11-10 20:57:12 +0100

    Added remaining token ids

--- src/scanner/scanner.h
+++ src/scanner/scanner.h
@@ -8,40 +8,46 @@
 #pragma once
 
 typedef enum tokenid_e {
-   //TOKENID_SPACE,
-   //TOKENID_INDENT,               // \t
    TOKENID_IDENTIFIER,
-   TOKENID_KEYWORD_IF,              // if
-   TOKENID_KEYWORD_FOR,             // for
-   TOKENID_KEYWORD_ELSE,            //else
-   TOKENID_KEYWORD_RETURN,          //return
-   TOKENID_KEYWORD_PACKAGE,         //package
-   TOKENID_KEYWORD_FUNC,            // func
-   TOKENID_KEYWORD_FLOAT64,         // flaot64
-   TOKENID_KEYWORD_INT,             // int
-   TOKENID_KEYWORD_STRING,          //string
-   TOKENID_KEYWORD_BOOL,            //bool
-   TOKENID_STRING_LITERAL,          // "string literal"
+   TOKENID_KEYWORD_IF,                 // if
+   TOKENID_KEYWORD_FOR,                // for
+   TOKENID_KEYWORD_ELSE,               //else
+   TOKENID_KEYWORD_RETURN,             //return
+   TOKENID_KEYWORD_PACKAGE,            //package
+   TOKENID_KEYWORD_FUNC,               // func
+   TOKENID_KEYWORD_FLOAT64,            // flaot64
+   TOKENID_KEYWORD_INT,                // int
+   TOKENID_KEYWORD_STRING,             //string
+   TOKENID_KEYWORD_BOOL,               //bool
+   TOKENID_STRING_LITERAL,             // "string literal"
    TOKENID_NUM,
    TOKENID_NUM_DECIMAL,
-   TOKENID_OPERATOR_PLUS,           // +
-   TOKENID_OPERATOR_MINUS,          // -
-   TOKENID_OPERATOR_MUL,            // *
-   TOKENID_OPERATOR_DIV,            // /
-   TOKENID_OPERATOR_LESS,           // <
-   TOKENID_OPERATOR_LESS_OR_EQUAL,  // <=
+   TOKENID_OPERATOR_ADD,               // +
+   TOKENID_OPERATOR_ADD_AND_ASSIGN,    // +=
+   TOKENID_OPERATOR_SUB,               // -
+   TOKENID_OPERATOR_SUB_AND_ASSIGN,    // -=
+   TOKENID_OPERATOR_MUL,               // *
+   TOKENID_OPERATOR_MUL_AND_ASSIGN,    // *=
+   TOKENID_OPERATOR_DIV,               // /
+   TOKENID_OPERATOR_DIV_AND_ASSIGN,    // -=
+   TOKENID_OPERATOR_LESS,              // <
+   TOKENID_OPERATOR_LESS_OR_EQUAL,     // <=
    TOKENID_OPERATOR_GREATER,           // >
    TOKENID_OPERATOR_GREATER_OR_EQUAL,  // >=
-   TOKENID_OPERATOR_DECLARE,          // :=
-   TOKENID_OPERATOR_ASSIGN,         // =
-   TOKENID_LEFT_PARENTHESES,        // (
-   TOKENID_RIGHT_PARENTHESES,       // )
-   TOKENID_LEFT_BRACKET,            // {
-   TOKENID_RIGHT_BRACKET,           // }
-   TOKENID_SEMICOLON,               // ;
-   TOKENID_COMMA,                   // ,
-   TOKENID_NEWLINE,                 // \n
-   TOKENID_END_OF_FILE              // EOF
+   TOKENID_OPERATOR_DECLARE,           // :=
+   TOKENID_OPERATOR_ASSIGN,            // =
+   TOKENID_OPERATOR_NOT,               // !
+   TOKENID_OPERATOR_NOT_EQUAL,         // !=
+   TOKENID_OPERATOR_AND,               // &&
+   TOKENID_OPERATOR_OR,                // ||
+   TOKENID_LEFT_PARENTHESES,           // (
+   TOKENID_RIGHT_PARENTHESES,          // )
+   TOKENID_LEFT_BRACKET,               // {
+   TOKENID_RIGHT_BRACKET,              // }
+   TOKENID_SEMICOLON,                  // ;
+   TOKENID_COMMA,                      // ,
+   TOKENID_NEWLINE,                    // \n
+   TOKENID_END_OF_FILE                 // EOF
 } tokenid_e;
 
 typedef union token_value_u {
