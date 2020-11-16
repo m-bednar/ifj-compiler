a8be875f023f504cd29e924e0737460e33906ec3
jackjack90s <xhavra18@stud.fit.vutbr.cz>
2020-11-16 11:51:15 +0100

    Added functions declarations in ntposymbol.h

--- src/parser/ntposymbol.h
+++ src/parser/ntposymbol.h
@@ -5,3 +5,31 @@
  */
 
 #pragma once
+
+#include <stdbool.h>
+#include "../memory.h"
+#include "../error.h"
+#include "../scanner/token.h"
+#include "ntsymbol.h"
+
+typedef enum symtype_e {
+    NONTERMINAL,
+    TERMINAL,
+    PRECEDENCE_OPERATOR
+} symtype_e;
+
+typedef struct ntposymbol_t {
+    symtype_e type;
+    int id;
+} ntposymbol_t;
+
+
+/**
+ * Constucts new terminal/nonterminal/precedence_operator symbol with given id.
+ */
+ntposymbol_t* ntposymbol_ctor(int id, symtype_e type);
+
+/**
+ * Destructs terminal/nonterminal/precedence_operator symbol and frees it's resources.
+ */
+void ntposymbol_dtor(ntposymbol_t* ntposymbol);
