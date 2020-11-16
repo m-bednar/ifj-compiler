4648311e112d4d942095da61d051d12c5e3a51c6
jackjack90s <xhavra18@stud.fit.vutbr.cz>
2020-11-16 11:42:44 +0100

    Created ntposymstack files

---
+++ src/parser/ntposymstack.c
@@ -0,0 +1,7 @@
+/**
+ * IFJ20 Compiler
+ * @file ntposymstack.c
+ * @authors Daniel Havranek (xhavra18)
+ */
+
+#include "ntposymstack.h"
---
+++ src/parser/ntposymstack.h
@@ -0,0 +1,7 @@
+/**
+ * IFJ20 Compiler
+ * @file ntposymstack.h
+ * @authors Daniel Havranek (xhavra18)
+ */
+
+#pragma once
