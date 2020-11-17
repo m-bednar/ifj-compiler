dd0630e60a3eb13a4877cf28c36c7908dc5f07f5
jackjack90s <xhavra18@stud.fit.vutbr.cz>
2020-11-17 08:48:37 +0100

    Moved precedence table to precedencetable.c

--- src/parser/precedencetable.c
+++ src/parser/precedencetable.c
@@ -4,4 +4,30 @@
  * @authors Daniel Havranek (xhavra18)
  */
 
-#pragma once
+#include "precedencetable.h"
+
+#define TABLE_SIZE 21
+
+const int precedence_table[TABLE_SIZE][TABLE_SIZE] = {
+    {G, L, L, L, L, L, L, L, L, L, L, L, L, L, G, L, L, L, L, L, G},
+    {G, G, L, L, L, L, L, L, L, L, L, L, L, L, G, L, L, L, L, L, G},
+    {G, G, G, G, L, L, L, L, L, L, L, L, L, L, G, L, L, L, L, L, G},
+    {G, G, G, G, L, L, L, L, L, L, L, L, L, L, G, L, L, L, L, L, G},
+    {G, G, G, G, G, G, G, G, L, L, L, L, L, L, G, L, L, L, N, L, G},
+    {G, G, G, G, G, G, G, G, L, L, L, L, L, L, G, L, L, L, N, L, G},
+    {G, G, G, G, G, G, G, G, L, L, L, L, L, L, G, L, L, L, N, L, G},
+    {G, G, G, G, G, G, G, G, L, L, L, L, L, L, G, L, L, L, N, L, G},
+    {G, G, G, G, G, G, G, G, G, G, L, L, L, L, G, L, L, L, N, L, G},
+    {G, G, G, G, G, G, G, G, G, G, L, L, L, L, G, L, L, L, N, N, G},
+    {G, G, G, G, G, G, G, G, G, G, G, G, L, L, G, L, L, L, N, N, G},
+    {G, G, G, G, G, G, G, G, G, G, G, G, L, L, G, L, L, L, N, N, G},
+    {G, G, G, G, G, G, G, G, G, G, G, G, L, L, G, L, L, L, L, N, G},
+    {L, L, L, L, L, L, L, L, L, L, L, L, L, L, E, L, L, L, L, L, N},
+    {G, G, G, G, G, G, G, G, G, G, G, G, N, N, G, N, N, N, N, N, G},
+    {G, G, G, G, G, G, G, G, G, G, G, G, N, N, G, N, N, N, N, N, G},
+    {G, G, G, G, G, G, G, G, G, G, G, G, N, N, G, N, N, N, N, N, G},
+    {G, G, G, G, G, G, G, G, G, G, G, G, N, N, G, N, N, N, N, N, G},
+    {G, G, G, G, N, N, N, N, N, N, N, N, N, N, G, N, N, N, N, N, G},
+    {G, G, G, G, G, G, G, G, G, N, N, N, N, N, G, N, N, N, N, N, G},
+    {L, L, L, L, L, L, L, L, L, L, L, L, L, L, N, L, L, L, L, L, N},
+};
--- src/parser/precedencetable.h
+++ src/parser/precedencetable.h
@@ -6,33 +6,6 @@
 
 #pragma once
 
-
-#define TABLE_SIZE 21
-
-static const int precedence_table[TABLE_SIZE][TABLE_SIZE] = {
-    {G, L, L, L, L, L, L, L, L, L, L, L, L, L, G, L, L, L, L, L, G},
-    {G, G, L, L, L, L, L, L, L, L, L, L, L, L, G, L, L, L, L, L, G},
-    {G, G, G, G, L, L, L, L, L, L, L, L, L, L, G, L, L, L, L, L, G},
-    {G, G, G, G, L, L, L, L, L, L, L, L, L, L, G, L, L, L, L, L, G},
-    {G, G, G, G, G, G, G, G, L, L, L, L, L, L, G, L, L, L, N, L, G},
-    {G, G, G, G, G, G, G, G, L, L, L, L, L, L, G, L, L, L, N, L, G},
-    {G, G, G, G, G, G, G, G, L, L, L, L, L, L, G, L, L, L, N, L, G},
-    {G, G, G, G, G, G, G, G, L, L, L, L, L, L, G, L, L, L, N, L, G},
-    {G, G, G, G, G, G, G, G, G, G, L, L, L, L, G, L, L, L, N, L, G},
-    {G, G, G, G, G, G, G, G, G, G, L, L, L, L, G, L, L, L, N, N, G},
-    {G, G, G, G, G, G, G, G, G, G, G, G, L, L, G, L, L, L, N, N, G},
-    {G, G, G, G, G, G, G, G, G, G, G, G, L, L, G, L, L, L, N, N, G},
-    {G, G, G, G, G, G, G, G, G, G, G, G, L, L, G, L, L, L, L, N, G},
-    {L, L, L, L, L, L, L, L, L, L, L, L, L, L, E, L, L, L, L, L, N},
-    {G, G, G, G, G, G, G, G, G, G, G, G, N, N, G, N, N, N, N, N, G},
-    {G, G, G, G, G, G, G, G, G, G, G, G, N, N, G, N, N, N, N, N, G},
-    {G, G, G, G, G, G, G, G, G, G, G, G, N, N, G, N, N, N, N, N, G},
-    {G, G, G, G, G, G, G, G, G, G, G, G, N, N, G, N, N, N, N, N, G},
-    {G, G, G, G, N, N, N, N, N, N, N, N, N, N, G, N, N, N, N, N, G},
-    {G, G, G, G, G, G, G, G, G, N, N, N, N, N, G, N, N, N, N, N, G},
-    {L, L, L, L, L, L, L, L, L, L, L, L, L, L, N, L, L, L, L, L, N},
-};
-
 typedef enum ptablesymbol_e {
     G,  // >
     L,  // <
@@ -63,6 +36,8 @@ typedef enum terminalid_e {
    TERMINAL_STRING_LITERAL,             // "string literal"
    TERMINAL_END_OF_FILE                 // EOF
 } terminalid_e;
+
+extern const int precedence_table;
 
 /**
  * Returns terminal ID of given token ID.
