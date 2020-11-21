3d3ae18f2b8cac4456c8f5c8c0fb0603b88b7811
Michal Trlica <garto25@gmail.com>
2020-11-22 00:15:17 +0100

    add ast ctors for all node types

--- src/parser/ast.c
+++ src/parser/ast.c
@@ -9,7 +9,52 @@
 
 astnode_t* ast_ctor(){
    astnode_t* ast = safe_alloc(sizeof(astnode_t));
+   ast->parent = NULL;
    ast->type = ANT_GLOBAL;
    ast->value.globalval.functions = NULL;
    return ast;
+}
+
+astnode_t* astnode_if_ctor(){
+   astnode_t* ast_node = safe_alloc(sizeof(astnode_t));
+   ast_node->parent = NULL;
+   ast_node->type = ANT_IF;
+   ast_node->value.ifval.condition = NULL;
+   ast_node->value.ifval.else_body = NULL;
+   ast_node->value.ifval.true_body = NULL;
+   return ast_node;
+}
+
+astnode_t* astnode_for_ctor(){
+   astnode_t* ast_node = safe_alloc(sizeof(astnode_t));
+   ast_node->parent = NULL;
+   ast_node->type = ANT_FOR;
+   ast_node->value.forval.body = NULL;
+   ast_node->value.forval.condition = NULL;
+   return ast_node;
+}
+
+astnode_t* astnode_exp_ctor(){
+   astnode_t* ast_node = safe_alloc(sizeof(astnode_t));
+   ast_node->parent = NULL;
+   ast_node->type = ANT_EXP;
+   ast_node->value.expval.postfix = NULL;
+   return ast_node;
+}
+
+astnode_t* astnode_ret_ctor(){
+   astnode_t* ast_node = safe_alloc(sizeof(astnode_t));
+   ast_node->parent = NULL;
+   ast_node->type = ANT_RET;
+   ast_node->value.returnval.expresion = NULL;
+   return ast_node;
+}
+
+astnode_t* astnode_func_ctor(){
+   astnode_t* ast_node = safe_alloc(sizeof(astnode_t));
+   ast_node->parent = NULL;
+   ast_node->type = ANT_FUNC;
+   ast_node->value.funcval.body = NULL;
+   ast_node->value.funcval.return_val = NULL;
+   return ast_node;
 }
