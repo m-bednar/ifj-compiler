fea00e1881655ad0f3abc6383c2a89327c3b5487
mar-bednar <mbedna8@seznam.cz>
2020-11-22 23:02:36 +0100

    Added generator files

---
+++ src/generator/generator.c
@@ -0,0 +1,9 @@
+/**
+ * IFJ20 Compiler
+ * @file generator.c
+ * @authors Martin Bednář (xbedna77)
+ */
+
+#include "generator.h"
+
+
---
+++ src/generator/generator.h
@@ -0,0 +1,7 @@
+/**
+ * IFJ20 Compiler
+ * @file generator.h
+ * @authors Martin Bednář (xbedna77)
+ */
+
+#pragma once
