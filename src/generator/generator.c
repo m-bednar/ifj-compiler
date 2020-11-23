21cdf06a7cacdb01f0227c31ec0a603402cff3f7
mar-bednar <mbedna8@seznam.cz>
2020-11-23 18:09:52 +0100

    Initial generator code

--- src/generator/generator.c
+++ src/generator/generator.c
@@ -5,7 +5,51 @@
  */
 
 #include "generator.h"
+#include <stdbool.h>
+#include <stdio.h>
 
+#define streq(a, b) !strcmp(a, b)
+#define printcm(s, ...) printf("   "); printf(s,##__VA_ARGS__); printf("\n")   // Print command function
+#define printlb(s, ...) printf("\n"); printf(s,##__VA_ARGS__); printf("\n")  // Print label function
+
+void generate_generic(astnode_generic_t* node) {
+   switch (node->type) {
+      case ANT_ASSIGN:
+         /* code */
+         break;
+      case ANT_DEFVAR:
+         /* code */
+         break;
+      case ANT_FOR:
+         /* code */
+         break;
+      case ANT_IF:
+         /* code */
+         break;
+      case ANT_FUNCCALL:
+         /* code */
+         break;
+      case ANT_RET:
+         /* code */
+         break;
+      default: 
+         exit(ERRCODE_INTERNAL_ERROR);
+   }
+}
+
+void generate_funcdecl(astnode_funcdecl_t* func) {
+   printlb("LABEL %s", func->name);
+   for (int i = 0; i < func->body->children_count; i++) {
+      generate_generic(func->body->children[i]);
+   }
+   printcm("RETURN");
+}
 
 void generate(astnode_global_t* global, bintree_t* symtable) {
+   printlb(".IFJcode20");
+   printcm("CALL main");
+   printcm("EXIT int@0");
+   for (int i = 0; i < global->functions_count; i++) {
+      generate_funcdecl(global->functions[i]);
+   }
 }
