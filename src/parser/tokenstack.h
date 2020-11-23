ae8214163c29e9a3d2426eb033f05c73a2d88b14
Michal Trlica <garto25@gmail.com>
2020-11-23 21:31:53 +0100

    add tokenstack

---
+++ src/parser/tokenstack.h
@@ -0,0 +1,23 @@
+/**
+ * IFJ20 Compiler
+ * @file ast.h
+ * @authors Michal Trlica (xtrlic02)
+ */
+
+#pragma once
+
+#include "../scanner/scanner.h"
+
+typedef struct tokenstack_t {
+   int length;
+   int capacity;
+   token_t** memory;
+} tokenstack_t;
+
+tokenstack_t* tokenstack_ctor();
+
+void tokenstack_dtor(tokenstack_t* stack);
+
+void tokenstack_push(tokenstack_t* stack, token_t* token);
+
+token_t* tokenstack_pop(tokenstack_t* stack);
