65798121bf4da09d433b84019e6a392b66d41538
mar-bednar <mbedna8@seznam.cz>
2020-11-26 10:46:49 +0100

    Comments added

--- src/generator/vartable.h
+++ src/generator/vartable.h
@@ -20,18 +20,43 @@ typedef struct vartable_t {
    vardata_t** data;
 } vartable_t;
 
+/**
+ * Allocates new vartable.
+ */
 vartable_t* vartable_ctor();
 
+/**
+ * Deallocates vartable.
+ */
 void vartable_dtor(vartable_t* vartable);
 
+/**
+ * Decreases vartable's current depth.
+ */
 void vartable_ascent(vartable_t* vartable);
 
+/**
+ * Increases vartable's current depth.
+ */
 void vartable_descent(vartable_t* vartable);
 
+/**
+ * Add new item to vartable.
+ */
 void vartable_add(vartable_t* vartable, char* identifier, vartype_e type);
 
+/**
+ * Returns found item depth.
+ */
 int vartable_depth(vartable_t* vartable, char* identifier);
 
+/**
+ * Returns true, if variable is not defined in current depth.
+ * If variable is defined in current scope, it's type would be replaced in vartable.
+ */
 bool vartable_should_define(vartable_t* vartable, char* identifier, vartype_e type);
 
+/**
+ * Returns variable appropriate to current depth.
+ */
 vardata_t* vartable_find(vartable_t* vartable, char* identifier);
