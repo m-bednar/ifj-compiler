170ebc1e526a7420ed53ed3efb40f023b7487406
Michal Trlica <garto25@gmail.com>
2020-11-26 22:35:09 +0100

    added astnodestack

---
+++ src/parser/astnodestack.h
@@ -0,0 +1,24 @@
+/**
+ * IFJ20 Compiler
+ * @file ast.c
+ * @authors Michal Trlica (xtrlic02)
+ */
+
+#include "ast.h"
+
+typedef struct astnodestack_t {
+   int length;
+   int capacity;
+   astnode_generic_t** memory;
+} astnodestack_t;
+
+astnodestack_t* astnodestack_ctor();
+
+void astnodestack_dtor(astnodestack_t* stack);
+
+astnode_generic_t* astnodestack_peek(astnodestack_t* stack);
+
+astnode_generic_t* astnodestack_pop(astnodestack_t* stack);
+
+void astnodestack_push(astnodestack_t* stack, astnode_generic_t* astnode);
+
