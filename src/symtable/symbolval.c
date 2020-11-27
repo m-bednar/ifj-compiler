add615bc6b2b71ec27fc3c18ce823e95f17fff42
Michal Trlica <garto25@gmail.com>
2020-11-28 00:58:46 +0100

    symbolval ctor update

--- src/symtable/symbolval.c
+++ src/symtable/symbolval.c
@@ -9,7 +9,7 @@
 #include "../error.h"
 #include "symbolval.h"
 
-symbolval_u symbolval_fn_ctor(int arg_count, int ret_count, vartype_e* arg_types, vartype_e* ret_types, bool defined) {
+symbolval_u symbolval_fn_ctor(int arg_count, int ret_count, char** arg_names, vartype_e* arg_types, vartype_e* ret_types, bool defined) {
    guard((arg_count == 0 && arg_types == NULL) || (arg_count != 0 && arg_types != NULL));
    guard((ret_count == 0 && ret_types == NULL) || (ret_count != 0 && ret_types != NULL));
    
@@ -18,6 +18,7 @@ symbolval_u symbolval_fn_ctor(int arg_count, int ret_count, vartype_e* arg_types
    
    fn->arg_count = arg_count;
    fn->arg_types = arg_types;
+   fn->arg_names = arg_names;
    fn->ret_count = ret_count;
    fn->ret_types = ret_types;
    fn->defined = defined;
