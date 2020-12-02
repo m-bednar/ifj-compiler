7539b6aefb967cb637f978f88008fe2f688de488
mar-bednar <mbedna8@seznam.cz>
2020-12-03 00:40:11 +0100

    Main updated

--- src/main.c
+++ src/main.c
@@ -9,11 +9,13 @@
 #include "parser/parser.h"
 #include "parser/ast.h"
 #include "symtable/symtable.h"
+#include "generator/generator.h"
 
 
 int main() {
    astnode_generic_t* ast = ast_ctor();
    bintree_t* symtable_global = bintree_ctor();
    parse(ast, symtable_global);
+   generate(ast->value.globalval, symtable_global);
    return EXIT_SUCCESS;
 }
